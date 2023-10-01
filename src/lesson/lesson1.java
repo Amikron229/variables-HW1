7761f638b368ee2ff372d0ae9e0cdc8c77de8bdf
Habib <timofeimatvei23@gmail.com>
2023-10-01 18:45:09 +0300

    Выполнил работу

---
+++ src/lesson/lesson1.java
@@ -0,0 +1,53 @@
+package lesson;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box / 3;
+        System.out.println(box);
+        System.out.println();
+        var liftingCapacity = 50;
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Еще можно положить " + capacityLeft + " кг вещей");
+
+        var appleWeight = 2;
+        var orangesWeight = 3;
+        var fruitWeight = appleWeight + orangesWeight;
+        System.out.println("Общий вес фруктов " + fruitWeight);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumbersWeight = 2;
+        var peppersWeight = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumbersWeight + peppersWeight + zucchiniWeight;
+        var productsWeight = fruitWeight + vegetablesWeight + meatWeight + waterWeight;
+        System.out.println("Общий вес продуктов " + productsWeight + " кг!");
+
+        var leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        System.out.println("Места осталось " + leftWeight + " кг!");
+
+        productsWeight = productsWeight * 2;
+        System.out.println("Теперь вес продуктов " + productsWeight);
+
+        leftWeight = liftingCapacity - stuffWeight - productsWeight;
+        System.out.println("Теперь место осталось " + leftWeight + " кг!");
+
+        var overLoad = (stuffWeight + productsWeight) % liftingCapacity;
+        System.out.println("Перегруз " + overLoad + " кг!");
+
+        var productsInOneCar = productsWeight / 2;
+        System.out.println("Продуктов в одной машине теперь " + productsInOneCar);
+    }
+}
